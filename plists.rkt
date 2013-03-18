57f39a1bd2adc752634803ae464226536e3cafb3
daniele capo <capo.daniele@gmail.com>
2013-03-18 15:15:47 +0100

    xexpr->dict and dict->xexpr

--- plists.rkt
+++ plists.rkt
@@ -14,7 +14,7 @@
   (match d
     [(dict entries) (cons 'dict (map dict->plist entries))]
     [(entry k v) (list 'assoc-pair (symbol->string k) (dict->plist v))]
-    [(? integer? n) (list 'integer n)]
+    [(? exact-integer? n) (list 'integer n)]
     [(? real? n) (list 'real n)]
     [(? string? s) s]
     [(list elts ...) (cons 'array (map dict->plist elts))]
@@ -32,6 +32,33 @@
     [(list 'false) #f]
     [(list 'true) #t]
     [(list 'array elts ...) (map plist->dict elts)]))
+
+(define (dict->xexpr d)
+  (match d
+    [(dict entries) (cons 'dict (cons null (foldr append '() (map dict->xexpr entries))))]
+    [(entry k v) 
+     (list (list 'key null (symbol->string k)) (dict->xexpr v))]
+    [(? exact-integer? n) (list 'integer null (number->string n))]
+    [(? real? n) (list 'real null (number->string n))]
+    [(? string? s) (list 'string null s)]
+    [(list elts ...) (cons 'array (cons null (map dict->xexpr elts)))]
+    [#f (list 'false null)]
+    [#t (list 'true null)]))
+
+(define (xexpr->dict x)
+  (match x
+    [(list 'dict null entries ...) (dict (xexpr->dict entries))]
+    [(list (list 'key null k) v entries ...) 
+     (cons (entry (string->symbol k) (xexpr->dict v))
+           (xexpr->dict entries))]
+    [(list 'integer null n) (string->number n)]
+    [(list 'real null n) (string->number n)]
+    [(list 'string null s) s]
+    [(list 'false null) #f]
+    [(list 'true null) #t]
+    [(list 'array null elts ...) (map xexpr->dict elts)]
+    [null null]))
+
 
 (define (dict->hashtable d)
   (define (process-entry e)
@@ -75,3 +102,11 @@
 
 (define (read-dict path)
   (plist->dict (call-with-input-file path read-plist)))
+
+
+;(define g (xml->xexpr 
+;   ((eliminate-whitespace '(glyph advance unicode image guideline anchor outline
+;                                  contour point component lib dict)
+;                          identity)
+;   (document-element
+;    (call-with-input-file "/Users/daniele/glif.glif" read-xml)))))
