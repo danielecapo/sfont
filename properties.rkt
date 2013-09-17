0e2d6d98cc8214fbc68b1d8164963f3ddcfc7db1
daniele capo <capo.daniele@gmail.com>
2013-09-17 15:19:58 +0200

    small formatting changes in properties

--- properties.rkt
+++ properties.rkt
@@ -4,23 +4,13 @@
 
 (provide (all-defined-out))
 
-
-;(define-values (prop:transform transform? transform-ref)
-;  (make-struct-type-property 'transform))
-      
-
-
-
-  
 ;;; property prop:has-matrix  
 ;;; Used for objects that have transformation matrix (like components)
-
 (define-values (prop:has-matrix has-matrix? has-matrix-ref)
   (make-struct-type-property 'has-matrix))
 
 ; Any -> TransfromationMatrix
 ; produce the TransformationMatrix of the object (if the (has-matrix? o) is true)
-
 (define (get-matrix o)
   (if (has-matrix? o)
       ((has-matrix-ref o) o)
@@ -28,7 +18,6 @@
 
 ; id Object(T) TransfromationMatrix -> Object(T)
 ; produce an object of type id with the new transformation matrix
-
 (define-syntax-rule (set-matrix id o m)
   (if (has-matrix? o)
       (struct-copy id o [matrix m])
@@ -36,13 +25,11 @@
 
 ;;; property prop:has-position  
 ;;; Used for objects that have a position
-
 (define-values (prop:has-position has-position? has-position-ref)
   (make-struct-type-property 'has-position))
 
 ; Any -> Vec
 ; produce the position Vec(tor) of the object
-
 (define (get-position o)
   (if (has-position? o)
       ((has-position-ref o) o)
@@ -50,7 +37,6 @@
 
 ; id Object(T) Vec -> Object(T)
 ; produce an object of type id with the new position
-
 (define-syntax-rule (set-position id o m)
   (if (has-position? o)
       (struct-copy id o [pos m])
